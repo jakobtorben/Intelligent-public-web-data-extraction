1a0fb84e95b5723c2ddfdda70c9c7baa0e4e6566
Yousef <namiyousef@hotmail.com>
2020-12-07 21:21:31 +0100

    #5 successfully extracted name and title of board members

--- Research/AstraZeneca/AstraZeneca_Spider/AstraZeneca_Spider/spiders/board_members.py
+++ Research/AstraZeneca/AstraZeneca_Spider/AstraZeneca_Spider/spiders/board_members.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import AstrazenecaSpiderItem
 
 class board_members(scrapy.Spider):
     """
@@ -15,7 +16,22 @@ class board_members(scrapy.Spider):
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = f'board_members-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        board_members = response.css('h2.bio__header')
+        print('===========================================================================================================================================')
+        for board_member in board_members:
+            name, title = board_member.css('span::text').getall()
+
+            items = AstrazenecaSpiderItem()
+            items['name'] = name
+            items['title'] = title
+
+            yield items
+        #filename = f'board_members-{page}.html'
+        #with open(filename, 'wb') as f:
+        #    f.write(board_members)
+        #self.log(f'Saved file {filename}')
+
+        #filename = f'board_members-{page}.html'
+        #with open(filename, 'wb') as f:
+        #    f.write(response.body)
+        #self.log(f'Saved file {filename}')
