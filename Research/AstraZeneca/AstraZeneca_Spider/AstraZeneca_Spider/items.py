252fb46ec1d939b230a438dd337a529feeaf5998
Yousef <namiyousef@hotmail.com>
2020-12-09 11:40:36 +0100

    minor setting modifications

--- Research/AstraZeneca/AstraZeneca_Spider/AstraZeneca_Spider/items.py
+++ Research/AstraZeneca/AstraZeneca_Spider/AstraZeneca_Spider/items.py
@@ -9,4 +9,5 @@ import scrapy
 class AstrazenecaSpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    title = scrapy.Field
