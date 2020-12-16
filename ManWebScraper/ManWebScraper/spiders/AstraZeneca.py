a80a4d2856babd4ed79e6662764469e60243d171
Yousef <namiyousef@hotmail.com>
2020-12-16 02:14:28 +0100

    #9 added sitemap.xml parser to find relevant change logs

--- ManWebScraper/ManWebScraper/spiders/AstraZeneca.py
+++ ManWebScraper/ManWebScraper/spiders/AstraZeneca.py
@@ -1,8 +1,17 @@
 import scrapy
 from ManWebScraper.items import Board
+import re
+from scrapy.spiders import XMLFeedSpider
+from scrapy.http import HtmlResponse
+from scrapy.http import Response
+from scrapy.selector import Selector
 
+"""
+To run this, simply use 'scrapy crawl AstraZeneca_board'
+"""
 
-class AstraZeneca_board(scrapy.Spider):
+class AstraZeneca_board(scrapy.Spider): # online solutions suggest that reading XML is best when you inherit XML class
+
     """
     Instantiates a 'spider' that can crawl the web.
     """
@@ -12,6 +21,27 @@ class AstraZeneca_board(scrapy.Spider):
     start_urls = [
         'https://www.astrazeneca.com/our-company/leadership.html'
     ]
+
+
+
+    def start_requests(self):
+        """
+        This method seems to be native to scrapy --> it allows you to set up a start sequence!
+        If unspecified, I believe scrapy will look for 'parse' method
+        """
+        yield scrapy.Request('https://www.astrazeneca.com/sitemap.xml', self.parse_XML)
+        yield scrapy.Request('https://www.astrazeneca.com/our-company/leadership.html',self.parse)
+
+    def parse_XML(self, response):
+        page = Selector(response) # this is useless, for some reason can't read the Xpath??
+        text = response.text
+        print(text)
+
+        text = text.replace("\n", "")
+        text = re.sub(' +', '', text)  # reduce multispacing to single spacing
+        print(re.findall(r'<url><loc>https://www.astrazeneca.com/our-company/leadership.html(.*?)</url>', text))
+
+        yield response
 
     def parse(self, response):
 
@@ -25,3 +55,10 @@ class AstraZeneca_board(scrapy.Spider):
             item['title'] = title[1:-1]
             print(name, title)
             yield item
+
+
+"""
+Useful links:
+https://www.youtube.com/watch?v=ALizgnSFTwQ
+
+"""
