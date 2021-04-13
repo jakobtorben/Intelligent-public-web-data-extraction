50d2b2dae9f7c4ad460af9d246504ae789748a55
Yousef <namiyousef@hotmail.com>
2021-04-13 12:16:49 +0200

    #32 adds functionality to collect meta, title text if CEO names found on page

--- IntelliLink/scrapy_navigator/scrapy_navigator/spiders/AstraZeneca.py
+++ IntelliLink/scrapy_navigator/scrapy_navigator/spiders/AstraZeneca.py
@@ -2,11 +2,87 @@ import scrapy
 import random
 import re
 from ..items import ScrapyNavigatorItem
+import pandas as pd
+
+
+path_to_people = '/Users/yousefnami/Desktop/Yousef/Imperial/Extra Curricular/ICDSS/Intelligent-public-web-data-extraction/ManWebScraper/AstraZeneca_board.csv'
+
+class AstraZenecaSpider(scrapy.Spider):
+    name = 'AstraZeneca'
+    # allowed_domains = ['wwAstraZeneca.com']
+    start_urls = ['https://www.astrazeneca.com']
+
+    # function that finds links on page and clicks them
+    def parse(self, response):
+        """ Collect links from the mainpage, currently does not support recursion """
+
+        for href in response.xpath("//a[not(contains(@href, '#'))]/@href"):
+            # TODO remove dupliate urls
+            url = response.urljoin(href.extract())
+
+            print(url)
+
+            #if url == 'https://www.astrazeneca.com/our-company.html':
+            yield scrapy.Request(url, callback = self.parse_tags)
+            #    break
+
+    def parse_test(self, response):
+
+        item = response.body
+        print(clean_html(str(item)))
+
+
+    # function that deals with pages returned from above
+    def parse_dir_contents(self, response):
+        item = ScrapyNavigatorItem()
+        for sel in response.xpath('//ul/li'):
+            # TODO @ricardo not sure what this is doing? is it doing parse again but at the
+            # TODO link level as opposed to mainpage? Not convincedim
+            print(sel)
+            item['link'] = sel.xpath('a/@href').extract()
+            print(item['link'])
+            yield item
+
+    def create_dataset(self, **kwargs):
+        item = ScrapyNavigatorItem()
+
+        # asign fields
+        for kw, arg in kwargs.items():
+            item[kw] = arg
+        return item
+
+    def parse_tags(self, response):
+        html_text = str(response.body)
+        # TODO clean this up, not very elegant..
+        # PATH to CSV containing names of CEOs and Board members
+
+        df = pd.read_csv('ENTER PATH')
+        names = list(df.name.values)
+        count = 0
+        save_names = []
+        for name in names:
+            name_components = name.split()
+            for component in name_components:
+                matches = re.findall(f"(?<![\w])(?i){component}(?![\w])", html_text)
+                if matches:
+                    count += len(matches)
+                    # TODO need to add smth to normalize the counts
+                    if name not in save_names:
+                        save_names.append(name)
+        if count:
+            title = response.css('title::text').get()
+            # TODO not sure if this will generalise? but seems standard?
+            meta = response.css('meta[name=description]::attr(content)').get()
+            yield self.create_dataset(link = response.request.url, count = count, names = save_names, title = title, meta= meta)
+        # TODO add code to get code from eikon?
+        # (for now just using simple pandas)
+
+    def parse_newspaper(self, ):
+        """ Some testing was done on newspaper library. Not successful."""
+        pass
 
 def clean_html(text):
-    #keep_letters = re.compile('[^a-zA-Z ]')
-
-    # TODO refine these
+    """ NOT RELEVANT ATM """
     # removes html tags
     cleaner = re.compile('<.*?>')
     text = re.sub(cleaner, '', text)
@@ -26,70 +102,11 @@ def clean_html(text):
     # shrinks any whitespace to single whitespace
     text = re.sub(r"\s+", " ", text)
 
-    #text = re.sub(keep_letters, '', text)
+    # text = re.sub(keep_letters, '', text)
 
-    #print(text)
-    #text = re.sub(r'[\\\/\+\(\-\*\)]', ' ', text)
-    #text =  re.sub(r"(?<!\_)[0-9]", '', text)
+    # print(text)
+    # text = re.sub(r'[\\\/\+\(\-\*\)]', ' ', text)
+    # text =  re.sub(r"(?<!\_)[0-9]", '', text)
     return text
 
 
-class AstraZenecaSpider(scrapy.Spider):
-    name = 'AstraZeneca'
-    # allowed_domains = ['wwAstraZeneca.com']
-    start_urls = ['https://www.astrazeneca.com']
-
-    # function that finds links on page and clicks them
-    def parse(self, response):
-        """ Creates start links for parse_dir_contents """
-
-        for href in response.xpath("//a[not(contains(@href, '#'))]/@href"):
-            # TODO make sure astrazeneca.com/ not duplicated?
-            # TODO in general need to remove duplicates
-            url = response.urljoin(href.extract())
-            if url == 'https://www.astrazeneca.com/our-company.html':
-                print(url)
-                yield scrapy.Request(url, callback = self.parse_test)
-            #reak
-
-
-    def parse_test(self, response):
-        item = response.body
-        print(clean_html(str(item)))
-                      #print(clean_html(item.extract_first()))
-
-    # function that deals with pages returned from above
-    def parse_dir_contents(self, response):
-        item = ScrapyNavigatorItem()
-        for sel in response.xpath('//ul/li'):
-            # TODO @ricardo not sure what this is doing? is it doing parse again but at the
-            # TODO link level as opposed to mainpage? Not convincedim
-            print(sel)
-            item['link'] = sel.xpath('a/@href').extract()
-            print(item['link'])
-            yield item
-    """
-    Find all the links on a page, choose one at random, follow that link and repeat.
-    This function is recursive and untested.
-
-
-    def parse_next_page(self, response):
-        # get all the links
-        for sel in response.xpath('//ul/li'):
-            item = ScrapyNavigatorItem()
-            # save the link
-            item['link'] = sel.xpath('a/@href').extract()
-            yield item
-
-        # list of all possible next pages
-        next_page = response.css("a::attr('href')")
-
-        # if there is a valid next page
-        if next_page:
-            # choose new page at random
-            selection = random.randint(0,len(next_page))
-            url = response.urljoin(next_page[selection].extract())
-            # use this function to parse it recursively
-            yield scrapy.Request(url, self.parse_next_page)
-
-    """
