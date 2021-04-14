ca383e055e9ea026efbc2d5f3194e4e2e7535f39
Jakob Torben <56504465+jakobtorben@users.noreply.github.com>
2021-04-14 13:43:11 +0200

    Add results from keyword method

--- IntelliLink/explore.py
+++ IntelliLink/explore.py
@@ -16,31 +16,52 @@ def text_from_html(html):
     visible_texts = filter(tag_visible, texts)
     return u" ".join(t.strip() for t in visible_texts).lower()
 
-root = "https://www.astrazeneca.com/"
-req = requests.get("https://www.astrazeneca.com/")
-root_soup = BeautifulSoup(req.text, 'html.parser')
-board_keyword = {"committee": 0, "board": 0, "officer": 0, "executive": 0, "leadership": 0,
-                 "non-executive": 0, "CEO": 0, "CFO" :0, "chief": 0, "director": 0, "directors": 0, "governace": 0}
-links = []
-relevance = []
+def explore_links(root, company, board_keyword):
+    req = requests.get(root)
+    root_soup = BeautifulSoup(req.text, 'html.parser')
+    links = []
+    relevance = []
 
-for anchor in root_soup.find_all('a', href=True):
-    url = anchor['href']
-    if url[0] == '/':
-        url = root + url
-    if url[0:4] != "http":
-        continue
-    print(url)
-    if url not in links:
-        html = requests.get(url)
-        text = text_from_html(html)
-        text = text.split()
-        for kword in board_keyword:
-            board_keyword[kword] = text.count(kword.lower())
-        links.append(url)
-        relevance.append(sum(board_keyword.values()))
+    for anchor in root_soup.find_all('a', href=True):
+        url = anchor['href']
+        if len(url) < 1:
+            continue
+        if url[0] == '/':
+            url = root + url
+        if url[0:4] != "http":
+            continue
+        if '#' in url:
+            continue
+        if "javascript:void(0)" in url:
+            continue
+        if "mailto" in url:
+            continue
+        if "pdf" in url:
+            continue
+        print(url)
+        if url not in links:
+            html = requests.get(url)
+            text = text_from_html(html)
+            text = text.split()
+            length = len(text)
+            if length == 0:
+                continue
+            for kword in board_keyword:
+                board_keyword[kword] = text.count(kword.lower())
+            links.append(url)
+            relevance.append(sum(board_keyword.values()) / length)
+
+    df = pd.DataFrame(data={"Link": links, "Relevance": relevance})
+    df = df.sort_values(by='Relevance', ascending=False)
+    df.to_csv(company + "_links.csv", index=False)
 
 
-df = pd.DataFrame(data={"Link": links, "Relevance": relevance})
-df = df.sort_values(by='Relevance', ascending=False)
-df.to_csv("./Astrazenaca_links.csv", index=False)
+board_keyword = {"title": 0, "director": 0, "board": 0, "executive": 0, "business": 0,
+                 "chief": 0, "group": 0, "global" :0, "officer": 0, "commmittee": 0, "member": 0,
+                 "governance": 0, "experience": 0}
+
+explore_links("https://www.reckitt.com/", "Reckitt", board_keyword)
+explore_links("https://www.compass-group.com/", "Compass Group", board_keyword)
+explore_links("https://www.lseg.com/", "London Stock Exchange Group", board_keyword)
+explore_links("https://www.hsbc.com/", "HSBC", board_keyword)
+explore_links("https://www.astrazeneca.com/", "Astrazenaca", board_keyword)
--- IntelliLink/keyword_db/keyword_db/spiders/database.py
+++ IntelliLink/keyword_db/keyword_db/spiders/database.py
@@ -13,9 +13,13 @@ from nltk.tokenize import word_tokenize
 class DatabaseSpider(scrapy.Spider):
     name = 'database'
 
+    def __init__(self):
+        self.global_text = ""
+        self.count = 0
+
     def start_requests(self):
         yield scrapy.Request('https://www.hsbc.com/who-we-are/leadership-and-governance/board-of-directors', callback = self.parse, meta = {"company": "HSBC"})
-        yield scrapy.Request('https://www.bhp.com/our-approach/our-company/leadership-team/', callback = self.parse, meta = {"company": "BHP Group"})
+        yield scrapy.Request('https://www.bhp.com/our-approach/our-company/leadership-team/', callback = self.parse, meta = {"company": "BHP Group"}) 
         yield scrapy.Request('http://www.unilever.com/about/who-we-are/our-leadership/', callback = self.parse, meta = {"company": "Unilever"})
         yield scrapy.Request('https://www.shell.com/about-us/leadership/executive-committee.html', callback = self.parse, meta = {"company": "Royal Dutch Shell"})
         yield scrapy.Request('https://www.riotinto.com/about/board-of-directors', callback = self.parse, meta = {"company": "Rio Tinto"})
@@ -33,12 +37,25 @@ class DatabaseSpider(scrapy.Spider):
         # complete text
         text = lxml.html.tostring(root, method="text", encoding='unicode')
         text = text.lower()
+        self.global_text += text
+        self.count += 1
 
+        # find most common word on one site
         text_tokens = word_tokenize(text)
         tokens_without_sw = [word for word in text_tokens if not word in stopwords.words() if word.isalpha()]
-
         dict_word = Counter(tokens_without_sw)
 
-        with open( response.meta.get('company') + '.csv', 'w') as csv_file:  
+        with open( response.meta.get('company') + '.csv', 'w', encoding="utf-8") as csv_file:  
             for key, value in dict_word.most_common():
-                 csv_file.write( "{}, {}\n".format(key,value) )
+                    csv_file.write( "{}, {}\n".format(key,value))
+
+        # find global sum
+        if (self.count == 10):
+            global_text_tokens = word_tokenize(self.global_text)
+            global_tokens_without_sw = [word for word in global_text_tokens if not word in stopwords.words() if word.isalpha()]
+            global_dict_word = Counter(global_tokens_without_sw)
+
+            with open('global_sum.csv', 'w', encoding="utf-8") as csv_file:  
+                for key, value in global_dict_word.most_common():
+                     csv_file.write( "{}, {}\n".format(key,value))
+
