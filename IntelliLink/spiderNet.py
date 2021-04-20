8cd298554276376695c1c85a70384c7a4f3fa564
ricardomokhtari <ricardomokhtari@gmail.com>
2021-04-20 23:54:02 +0100

    adds spidernet

---
+++ IntelliLink/spiderNet.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn as nn
+
+# replay buffer class stores transition information (s,a,r,s') dynamically
+# not used in testing
+class ReplayBuffer:
+    
+    def __init__(self):
+        self.buffer_len = 32
+        self.buffer = collections.deque(maxlen=5000)
+        
+    def append_to_buffer(self,transition):
+        self.buffer.append(transition)
+        
+    def sample_minibatch(self):
+        return random.sample(self.buffer,self.buffer_len)
+
+# basic network architecture
+# 3 layers: input, hidden, output
+# relu activation, 100 nodes in hidden layer
+class sampleNetwork(nn.Module):
+    
+    def __init__(self, input_dimension, output_dimension):
+        super(sampleNetwork, self).__init__()
+        self.layer_1 = nn.Linear(input_dimension, 100)
+        self.layer_2 = nn.Linear(100, 100)
+        self.output_layer = nn.Linear(100, output_dimension)
+        
+    def forward(self, x):
+        x = F.relu(self.layer_1(x))
+        x = F.relu(self.layer_2(x))
+        x = self.output_layer(x)
+        return x
+
+# driver class
+class spiderNet:
+    
+    def __init__(self,input_dimension):
+        self.network = sampleNetwork(input_dimension, output_dimension=1)
+        self.optimiser = optim.Adam(self.network.parameters(), lr=0.01)
+    
+    # training on a minibatch
+    # 'Q_true' is only there for testing, would be replaced with a 
+    # Q estimate in real implementation
+    def train(self, minibatch, Q_true):
+        self.optimiser.zero_grad()
+        loss = self._calculate_loss(minibatch, Q_true)
+        loss.backward()
+        self.optimiser.step()
+        return loss.item()
+    
+    # calculate loss on a minibatch, again Q_true only there for testing
+    def _calculate_loss(self, minibatch, Q_true):
+        
+        Q_pred = self.network.forward(minibatch)
+        
+        # calculate MSE loss
+        loss = torch.nn.MSELoss()(Q_pred,Q_true)
+        
+        return loss
